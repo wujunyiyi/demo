ba52d3eb8031cd383e85df82025fc7964f34e885
za-wujunyi <junyi.wu@zatech.com>
2022-08-18 10:33:21 +0800

    初始化demo

---
+++ algorithm/src/test/java/SortTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author: junyi.wu
+ * @date: 2022/8/18
+ */
+public class SortTest {
+
+    @Test
+    public void test() {
+        System.out.println(1);
+    }
+}
