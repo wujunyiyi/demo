17bc8914f253ce1f3463cb7e4ab85cb05c91de99
junyi.wu <junyi.wu@zatech.com>
2022-09-28 14:14:39 +0800

    leetcode

---
+++ algorithm/src/test/java/leetcode/Solution1709I.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+/**
+ * 有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。注意，不是必须有这些素因子，而是必须不包含其他的素因子。例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: k = 5
+ * <p>
+ * 输出: 9
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/get-kth-magic-number-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author junyi.wu
+ * @date 2022/9/28
+ */
+public class Solution1709I {
+
+    /**
+     * 没看懂题目。  (叶师傅题解)
+     *
+     * @param k
+     * @return
+     */
+    public int getKthMagicNumber(int k) {
+        int[] ans = new int[k + 1];
+        ans[1] = 1;
+        for (int i3 = 1, i5 = 1, i7 = 1, idx = 2; idx <= k; idx++) {
+            int a = ans[i3] * 3, b = ans[i5] * 5, c = ans[i7] * 7;
+            int min = Math.min(a, Math.min(b, c));
+            if (min == a) i3++;
+            if (min == b) i5++;
+            if (min == c) i7++;
+            ans[idx] = min;
+        }
+        return ans[k];
+    }
+}
