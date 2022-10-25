5588cbcbcaf0fbde09f85839ed349e962d044c9d
junyi.wu <junyi.wu@zatech.com>
2022-10-25 16:39:01 +0800

    leetcode

--- util/src/test/java/LocalUtil.java
+++ util/src/test/java/LocalUtil.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.Locale;
 
 import org.apache.commons.lang3.LocaleUtils;
@@ -13,5 +14,19 @@ public class LocalUtil {
     public void test() {
         Locale zh_cn = LocaleUtils.toLocale("ja_pn");
         System.out.println(zh_cn);
+    }
+
+    @Test
+    public void test1() {
+        BigDecimal bigDecimal = new BigDecimal(1);
+        BigDecimal ans = bigDecimal.add(new BigDecimal(-1));
+        System.out.println(bigDecimal);
+        System.out.println(ans);
+        System.out.println(bigDecimal.negate());
+    }
+
+    @Test
+    public void test2t(){
+        System.out.println(System.getenv("hostname"));
     }
 }
